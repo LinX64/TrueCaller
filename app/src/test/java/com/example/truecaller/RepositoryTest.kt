a67552f665d7f49ebff3d4abf8f0a4d34b89bfc8
Mohsen <ash.wxrz@hotmail.com>
2022-09-15 01:47:03 +0200

    Added the unit tests / using runTest

--- app/src/test/java/com/example/truecaller/RepositoryTest.kt
+++ app/src/test/java/com/example/truecaller/RepositoryTest.kt
@@ -2,115 +2,97 @@ package com.example.truecaller
 
 import com.example.truecaller.data.api.ApiService
 import com.example.truecaller.data.repository.MainRepository
-import com.example.truecaller.util.DataHelper
 import com.example.truecaller.util.ResponseHandler
 import com.example.truecaller.util.getBodyUsingSplit
+import com.example.truecaller.util.getHtmlResponse
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import okhttp3.ResponseBody
+import kotlinx.coroutines.test.runTest
 import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
+@RunWith(JUnit4::class)
 class RepositoryTest {
+
     private val mockHandler = mock<ResponseHandler>()
+    private lateinit var mainRepository: MainRepository
+    private lateinit var apiService: ApiService
 
+    @Before
+    fun setUp() {
+        apiService = mockApiService()
+        mainRepository = MainRepository(apiService, mockHandler)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `GIVEN the html file THEN extract 10th character correctly`(): Unit = runBlocking {
+    fun `GIVEN the html file THEN extract 10th character correctly`() = runTest {
         // given
-        val mockResponse = DataHelper.getHtmlResponse().toResponseBody()
-        val mockApiService = mockGet10Char(mockResponse)
-        val mainRepository = MainRepository(mockApiService, mockHandler)
-
-        val expectedChar = "p"
+        val mRepository = mainRepository.getTrueCallerData()
 
         // when
-        val mRepo = mainRepository.getTrueCaller10thCharacterRequest().data?.string()
-        val mBody = getBodyUsingSplit(mRepo!!)
-        val actualChar = mBody[10].toString()
+        val expectedChar = "p"
+        val getBody = mRepository.data?.string()?.let { getBodyUsingSplit(it) }?.trim()
+        val actualChar = getBody?.get(10).toString()
 
         // then
         assertEquals(expectedChar, actualChar)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `WHEN the html file THEN return every 10th character`(): Unit = runBlocking {
+    fun `WHEN the html file THEN return every 10th character`() = runTest {
         // given
-        val mockResponse = DataHelper.getHtmlResponse().toResponseBody()
-        val mockApiService = mockGet10Char(mockResponse)
-        val mainRepository = MainRepository(mockApiService, mockHandler)
+        val mRepository = mainRepository.getTrueCallerData()
 
+        // when
         val expected10thChar = "p"
         val expected20thChar = "e"
 
-        // when
-        val mRepo = mainRepository.getTrueCaller10thCharacterRequest().data?.string()
-        val mBody = getBodyUsingSplit(mRepo!!)
-        val actual10thChar = mBody[10].toString()
-        val actual20thChar = mBody[20].toString()
+        val getBody = mRepository.data?.string()?.let { getBodyUsingSplit(it) }?.trim()
+        val actual10thChar = getBody?.get(10).toString()
+        val actual20Char = getBody?.get(20).toString()
 
         // then
         assertEquals(expected10thChar, actual10thChar)
-        assertEquals(expected20thChar, actual20thChar)
+        assertEquals(expected20thChar, actual20Char)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `WHEN the html file THEN return Word Counter string`(): Unit = runBlocking {
+    fun `WHEN the html file THEN return Word Counter string`() = runTest {
         // given
-        val mockResponse = DataHelper.getHtmlResponse().toResponseBody()
-        val mockApiService = mockGet10Char(mockResponse)
-        val mainRepository = MainRepository(mockApiService, mockHandler)
-
-        val expectedChars = "".toResponseBody()
+        val mRepository = mainRepository.getTrueCallerData()
 
         // when
-        val mRepo = mainRepository.getTrueCallerWordCounterRequest().data
+        val getBody = mRepository.data?.string()?.let { getBodyUsingSplit(it) }?.trim()
+        val words = getBody?.split("\\s+".toRegex())
+        val expectedFirstChar = "!<md:absolute"
 
         // then
-        whenever(mRepo).thenReturn(expectedChars)
-    }
-
-    @Test
-    fun `WHEN the html file is empty THEN return empty string`(): Unit = runBlocking {
-        // given
-        val mockResponse = DataHelper.getHtmlResponse().toResponseBody()
-        val mockApiService = mockGet10Char(mockResponse)
-        val mainRepository = MainRepository(mockApiService, mockHandler)
-
-        val expectedChar = "".toResponseBody()
-
-        // when
-        val mRepo = mainRepository.getTrueCaller10thCharacterRequest().data
-
-        // then
-        whenever(mRepo).thenReturn(expectedChar)
-    }
-
-    private fun mockGet10Char(res: ResponseBody): ApiService =
-        runBlocking {
-            return@runBlocking mock<ApiService> {
-                onBlocking {
-                    getTrueCaller10thCharacterRequest()
-                } doReturn res
-            }
+        val result = mutableMapOf<String, Int>().toSortedMap()
+        words?.forEach { word ->
+            if (result.containsKey(word))
+                result[word] = result[word]!! + 1
+            else result[word] = 1
         }
 
-    private fun mockGetEvery10Char(res: ResponseBody): ApiService =
-        runBlocking {
-            return@runBlocking mock<ApiService> {
-                onBlocking {
-                    getTrueCallerEvery10thCharacterRequest()
-                } doReturn res
-            }
-        }
+        val getFirstItem = result.firstKey()
+        assertEquals(expectedFirstChar, getFirstItem)
+    }
 
-    private fun mockGetWordCounter(res: ResponseBody): ApiService =
+    private fun mockApiService(): ApiService =
         runBlocking {
             return@runBlocking mock<ApiService> {
                 onBlocking {
-                    getTrueCallerWordCounterRequest()
-                } doReturn res
+                    getTrueCallerData()
+                } doReturn getHtmlResponse().toResponseBody()
             }
         }
 }
