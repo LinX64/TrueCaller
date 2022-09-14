234b7c46346d6c5c2027c72b5fa4e382599f797a
Mohsen <ash.wxrz@hotmail.com>
2022-09-15 01:47:22 +0200

    Replaced the companion object with extension function

--- app/src/test/java/com/example/truecaller/util/DataHelper.kt
+++ app/src/test/java/com/example/truecaller/util/DataHelper.kt
@@ -1,17 +1,5 @@
 package com.example.truecaller.util
 
-class DataHelper {
+fun getHtmlResponse() = ClassLoader.getSystemResource("file.html").readText()
 
-    companion object {
-        fun getHtmlResponse(): String {
-            return ClassLoader.getSystemResource("file.html").readText()
-        }
-
-        fun getHtmlBody(mData: String): String {
-            return mData.split("<body>")[1].split("</body>")[0]
-        }
-
-        fun getBodyUsingSplit(getBody: String) =
-            getBody.split("<body>")[1].split("</body>")[0]
-    }
-}
+fun getBodyUsingSplit(getBody: String) = getBody.split("<body>")[1].split("</body>")[0]
